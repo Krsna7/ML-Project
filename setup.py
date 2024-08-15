7c771d6ee180b6e2ad5aec8f4597548bcf11cb0d
Krsna7 <krsna1123@gmail.com>
2024-08-15 11:54:41 +0530

    Model Trainer and Hyper Parameter Tuning

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path: str) -> List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Krish',
+    author='Krishna',
     author_email='krsna1123@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
